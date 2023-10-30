dbb53813bbfdfa357f22a35a9f579c1d41fcba4e
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-10-30 13:40:45 +0200

    Add project files.

---
+++ DataBase in WinForms Lmao1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataBase_in_WinForms_Lmao1
+{
+    public partial class Form1 : Form
+    {
+        DataBase dataBase = new DataBase();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "DESKTOP-4SK39GD";
+            builder.InitialCatalog = "ITacademy";
+            builder.UserID = textBox1.Text;
+            builder.Password = textBox2.Text;
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Підключено до бази даних MyDB успішно!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка підключення: " + ex.Message);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//login - Slava
+//password - Ukraine
